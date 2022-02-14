21f567a09216df08b9a765544d3ee407659d1d29
eulices-garces <ulifreelancer@gmail.com>
2022-02-14 06:24:44 -0500

    react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import React from 'react';
-//Components
+// Routing
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// Components
 import Header from './componets/Header';
 import Home from './componets/Home';
+import Movie from './componets/Movie';
+import NotFound from './componets/NotFound';
 
 //Styles
 import { GlobalStyle } from './GlobalStyle';
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Home />
-      <GlobalStyle />
-    </div>
-  );
-}
+const App = () => (
+  <Router>
+    <Header />
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/:movieId' element={<Movie />} />
+      <Route path='/*' element={<NotFound />} />
+    </Routes>
+    <GlobalStyle />
+  </Router>
+);
+
 
 export default App;
